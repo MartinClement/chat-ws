6f2ca4369c7266dc4593b4b56e54b880022a0d8d
Clément Martin <clement@livemon.com>
2018-12-22 00:03:43 +0100

    ES6 syntax and fix bugs

--- chat-ws.js
+++ chat-ws.js
@@ -1,89 +1,74 @@
 "use strict";
 
-var http = require('http');
-/////// FIX OPENODE ISSUE ///
 
-const server = http.createServer(function (req, res) {
-	res.end("Hello!");
-})
+// const http = require("http");
 
-server.listen(80, (err) => {
-	if ( ! err) {
-		console.log(`server is listening on 80`)
-	}
-})
+// const server = http.createServer(function (req, res) {
+// 	res.end("Hello!");
+// })
 
-////////
+// server.listen(80, (err) => {
+// 	if ( ! err) {
+// 		console.log(`server is listening on 80`)
+// 	}
+// })
 
-// Optional. You will see this name in eg. 'ps' or 'top' command
-process.title = 'chat-ws';
 
-// Port where we'll run the websocket server
-var webSocketsServerPort = 80;
-var port = normalizePort(process.env.PORT || '8080');
+const http = require("http");
 
 // websocket and http servers
 var webSocketServer = require('websocket').server;
 
 
-/**
- * Global variables
- */
-// latest 100 messages
 var history = [ ];
-// list of currently connected clients (users)
 var clients = [ ];
-/**
- * Helper function for escaping input strings
- */
+
 function htmlEntities(str) {
   return String(str)
       .replace(/&/g, '&amp;').replace(/</g, '&lt;')
       .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
-// Array with some colors
 var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
-
-// ... in random order
 colors.sort(function(a,b) { return Math.random() > 0.5; } );
 
-/**
- * HTTP server
- */
-var server = http.createServer(function(request, response) {
-  // Not important for us. We're writing WebSocket server,
-  // not HTTP server
+// /**
+//  * HTTP server
+//  */
+const server = http.createServer((req, res) => {
+  res.end("Hello!");
 });
 
-server.listen(port, function() {
-  console.log((new Date()) + " Server is listening on port "
-      + webSocketsServerPort);
+server.listen(80, (err) => {
+  if ( ! err ) {
+        console.log((new Date()) + " Server is listening on port "
+      + 80);
+  }
 });
 
 /**
  * WebSocket server
  */
-var wsServer = new webSocketServer({
+const wsServer = new webSocketServer({
   // WebSocket server is tied to a HTTP server. WebSocket
   // request is just an enhanced HTTP request. For more info 
   // http://tools.ietf.org/html/rfc6455#page-6
   httpServer: server
 });
 
-// This callback function is called every time someone
-// tries to connect to the WebSocket server
-wsServer.on('request', function(request) {
+// // This callback function is called every time someone
+// // tries to connect to the WebSocket server
+wsServer.on('request', (request) => {
   console.log((new Date()) + ' Connection from origin '
       + request.origin + '.');
 
   // accept connection - you should check 'request.origin' to
   // make sure that client is connecting from your website
   // (http://en.wikipedia.org/wiki/Same_origin_policy)
-  var connection = request.accept(null, request.origin);
+  const connection = request.accept(null, request.origin);
 
   // we need to know client index to remove them on 'close' event
-  var index = clients.push(connection) - 1;
+  const index = clients.push(connection) - 1;
   
   var userName = false;
   var userColor = false;
@@ -96,8 +81,8 @@ wsServer.on('request', function(request) {
         JSON.stringify({ type: 'chatHistory', data: history} ));
   }
 
-  // user sent some message
-  connection.on('message', function(message) {
+  //user sent some message
+  connection.on('message', (message) => {
     if (message.type === 'utf8') { // accept only text
     // first message sent by user is their name
      if (userName === false) {
@@ -115,7 +100,7 @@ wsServer.on('request', function(request) {
                     + userName + ': ' + message.utf8Data);
         
         // we want to keep history of all sent messages
-        var obj = {
+        const obj = {
           time: (new Date()).getTime(),
           text: htmlEntities(message.utf8Data),
           author: userName,
@@ -125,16 +110,14 @@ wsServer.on('request', function(request) {
         history = history.slice(-100);
 
         // broadcast message to all connected clients
-        var json = JSON.stringify({ type:'message', data: obj });
-        for (var i=0; i < clients.length; i++) {
-          clients[i].sendUTF(json);
-        }
+        const json = JSON.stringify({ type:'message', data: obj });
+        clients.map(c => c.sendUTF(json))
       }
     }
   });
 
   // user disconnected
-  connection.on('close', function(connection) {
+  connection.on('close', (connection) => {
     if (userName !== false && userColor !== false) {
       console.log((new Date()) + " Peer "
           + connection.remoteAddress + " disconnected.");
